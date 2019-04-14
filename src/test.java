6e4eb9ac6f164d24f65da4e056cdad52d637d05f
vic <1469925734@qq.com>
2019-04-14 16:03:40 +0800

    Idea整合GitHub测试

---
+++ src/test.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.text.*;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+    idk idk=new idk();
+    idk.f(1,2,3,4,5);
+
+
+    }
+
+
+}
+class idk{
+    public void f(int... i){
+        System.out.println(i[5]);
+    }
+}
+
